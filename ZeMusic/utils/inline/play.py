bf380dc34944196b712e2f7ff37182731007ac9a
yousef1yt <167021222+yousef1yt@users.noreply.github.com>
2024-05-08 01:50:21 +0300

    Update play.py

--- ZeMusic/utils/inline/play.py
+++ ZeMusic/utils/inline/play.py
@@ -64,7 +64,7 @@ def stream_markup_timer(_, chat_id, played, dur):
             InlineKeyboardButton(text="𖣂 ايقاف 𖣂", callback_data=f"ADMIN Stop|{chat_id}"),
         ],[
             InlineKeyboardButton(text="⏤͟͞ َِ⍣⃟َ𝐓𝐇ِ𝐄 𓆩ََِِ𝐘َِْ𝐎ِِ𝐔ًٌُ𝐒𝐄ُِ𝐅𓆪", url=f"https://t.me/y_o_v"),
-                        ],[
+                        
             InlineKeyboardButton(text=config.CHANNEL_NAME, url=lnk)],
     ]
         ],[
@@ -84,7 +84,7 @@ def stream_markup(_, chat_id):
             InlineKeyboardButton(text="𖣂 ايقاف 𖣂", callback_data=f"ADMIN Stop|{chat_id}"),
         ],[
             InlineKeyboardButton(text="⏤͟͞ َِ⍣⃟َ𝐓𝐇ِ𝐄 𓆩ََِِ𝐘َِْ𝐎ِِ𝐔ًٌُ𝐒𝐄ُِ𝐅𓆪", url=f"https://t.me/y_o_v"),
-                ],[
+                
             InlineKeyboardButton(text=config.CHANNEL_NAME, url=lnk)],
     ]
         ],[
