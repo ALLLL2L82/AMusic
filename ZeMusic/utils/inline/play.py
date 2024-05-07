473877ed7c90caaa177ff19c70ffd84ee41565c0
yousef1yt <167021222+yousef1yt@users.noreply.github.com>
2024-05-08 02:35:33 +0300

    Update play.py

--- ZeMusic/utils/inline/play.py
+++ ZeMusic/utils/inline/play.py
@@ -60,8 +60,7 @@ def stream_markup_timer(_, chat_id, played, dur):
             InlineKeyboardButton(text="𖣂 ايقاف مؤقت 𖣂", callback_data=f"ADMIN Pause|{chat_id}"),
             InlineKeyboardButton(text="𖣂 اعاده 𖣂", callback_data=f"ADMIN Replay|{chat_id}"),
         ],[
-            InlineKeyboardButton(text="𖣂 تخطي 𖣂", callback_data=f"ADMIN Skip|{chat_id}"),
-            InlineKeyboardButton(text="𖣂 توقف 𖣂", callback_data=f"ADMIN Stop|{chat_id}"),
+            InlineKeyboardButton(text=config.CHANNEL_NAME, url=config.CHANNEL_LINK)],
         ],[
             InlineKeyboardButton(text="⏤͟͞ َِ⍣⃟َ𝐓𝐇ِ𝐄 𓆩ََِِ𝐘َِْ𝐎ِِ𝐔ًٌُ𝐒𝐄ُِ𝐅𓆪", url=f"https://t.me/y_o_v"),
         ],[
@@ -74,11 +73,10 @@ def stream_markup(_, chat_id):
     buttons = [
         [
             InlineKeyboardButton(text="𖣂 استئناف 𖣂", callback_data=f"ADMIN Resume|{chat_id}"),
-            InlineKeyboardButton(text="𖣂 ليقاف مؤقت 𖣂", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="𖣂 ايقاف مؤقت 𖣂", callback_data=f"ADMIN Pause|{chat_id}"),
             InlineKeyboardButton(text="𖣂 اعاده 𖣂", callback_data=f"ADMIN Replay|{chat_id}"),
         ],[
-            InlineKeyboardButton(text="𖣂 تخطي 𖣂", callback_data=f"ADMIN Skip|{chat_id}"),
-            InlineKeyboardButton(text="𖣂 توقف 𖣂", callback_data=f"ADMIN Stop|{chat_id}"),
+            InlineKeyboardButton(text=config.CHANNEL_NAME, url=config.CHANNEL_LINK)],
         ],[
             InlineKeyboardButton(text="⏤͟͞ َِ⍣⃟َ𝐓𝐇ِ𝐄 𓆩ََِِ𝐘َِْ𝐎ِِ𝐔ًٌُ𝐒𝐄ُِ𝐅𓆪", url=f"https://t.me/y_o_v"),
         ],[
